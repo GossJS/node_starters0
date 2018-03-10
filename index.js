295553fa10816c672f2c6412caa92c2325663fcb
Ilya Goss <igossoudarev@herzen.spb.ru>
2018-03-10 19:38:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const http = require('http');
 const moment = require('moment');
 
-http.createServer((req, res) => {
+const server = http.createServer();
+server.listen(4321);
+server.on('request', (req, res) => {
   res.setHeader('Content-Type', 'application/json; charset=utf-8');
   res.end(JSON.stringify({ date: moment().format('DD.MM.YYYY HH:mm:ss') }));
-}).listen(4321);
+});
